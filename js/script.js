42bff575e8cf4e6af4b8a6cd7fdf73bdefd59f64
Victoria <84863477+Victoria828@users.noreply.github.com>
2023-05-01 19:59:11 +0200

    нова версія 1 травня

--- js/script.js
+++ js/script.js
@@ -15,9 +15,6 @@ let secondDate = document.querySelector(".end-date");
 let resultTable = document.querySelector(".result-table");
 let rows = resultTable.rows;
 
-let firstValue = new Date(document.querySelector(".start-date").value);
-let endValue = new Date(document.querySelector(".end-date").value);
-
 let week = document.querySelector(".week");
 let month = document.querySelector(".month");
 
@@ -25,31 +22,34 @@ let buttonCount = document.querySelector(".button-count");
 
 let output = document.querySelector("output");
 
+let typeDate = document.querySelector(".type_date");
+
 // Пресети
 
 //Тиждень
 
-week.onclick = function () {
+week.addEventListener("click", function () {
   let startData = new Date(firstDate.value);
   let endData = new Date(startData);
   endData.setDate(startData.getDate() + 7);
   secondDate.valueAsDate = endData;
   buttonCount.disabled = false;
-};
+});
+
 //Місяць
 
-month.onclick = function () {
+month.addEventListener("click", function () {
   let startData = new Date(firstDate.value);
   let endData = new Date(startData);
   endData.setMonth(startData.getMonth() + 1);
   secondDate.valueAsDate = endData;
   buttonCount.disabled = false;
-};
+});
 
 //Ввід дати
 
 //Початкова дата
-firstDate.onchange = function () {
+firstDate.addEventListener("change", function () {
   if (firstDate.value === "") {
     secondDate.disabled = true;
     buttonCount.disabled = true;
@@ -57,45 +57,115 @@ firstDate.onchange = function () {
     secondDate.disabled = false;
     secondDate.setAttribute("min", this.value);
   }
-};
+});
 
 //Кінцева дата
-secondDate.onchange = function () {
+secondDate.addEventListener("change", function () {
   if (secondDate.value === "") {
     buttonCount.disabled = true;
   } else {
     buttonCount.disabled = false;
   }
   firstDate.setAttribute("max", this.value);
-};
+});
 
 //Функція розрахування
 
-buttonCount.onclick = function calculateDateDifference() {
-  if (document.querySelector(".type_date").value === "weekday") {
-    function getWorkingDays(start, end, type) {
-      let result = 0;
-      while (start <= end) {
-        let weekDay = start.getDay();
-        if (weekDay != 0 && weekDay != 6) {
-          result++;
-        }
-        start.setDate(start.getDate() + 1);
-      }
-      switch (type) {
-        case "hours":
-          result = result * HOURS_IN_DAY;
-          break;
-        case "minutes":
-          result = result * MINUTES_IN_DAY;
-          break;
-        case "seconds":
-          result = result * SECONDS_IN_DAY;
-          break;
-      }
-      return `${result} ${type}`;
-    }
+//Перевірка на вихідний день
 
+function isWeekend(date) {
+  let newDate = new Date(date);
+  let weekDay = newDate.getDay();
+
+  if (weekDay === 0 || weekDay === 6) {
+    return true;
+  } else {
+    return false;
+  }
+  return weekDay === 0 || weekDay === 6;
+}
+
+//Всі дні
+
+function durationBetweenDates(start, end, type) {
+  let x = new Date(start);
+  let y = new Date(end);
+
+  let diff = y.getTime() - x.getTime();
+  let result;
+
+  switch (type) {
+    case "days":
+      result = diff / DAY_IN_MILLISECONDS + 1;
+      break;
+    case "hours":
+      result = diff / HOUR_IN_MILLISECONDS + HOURS_IN_DAY;
+      break;
+    case "minutes":
+      result = diff / MINUTE_IN_MILLISECONDS + MINUTES_IN_DAY;
+      break;
+    case "seconds":
+      result = diff / SECOND_IN_MILLISECONDS + SECONDS_IN_DAY;
+      break;
+  }
+
+  return `${result} ${type}`;
+}
+
+//Вихідні дні
+
+function getWeekends(start, end, type) {
+  let result = 0;
+  while (start <= end) {
+    let weekDay = start.getDay();
+    if (isWeekend(start) === true) {
+      result++;
+    }
+    start.setDate(start.getDate() + 1);
+  }
+  switch (type) {
+    case "hours":
+      result = result * HOURS_IN_DAY;
+      break;
+    case "minutes":
+      result = result * MINUTES_IN_DAY;
+      break;
+    case "seconds":
+      result = result * SECONDS_IN_DAY;
+      break;
+  }
+  return `${result} ${type}`;
+}
+
+//Будні дні
+
+function getWorkingDays(start, end, type) {
+  let result = 0;
+  while (start <= end) {
+    let weekDay = start.getDay();
+    if (isWeekend(start) === false) {
+      result++;
+    }
+    start.setDate(start.getDate() + 1);
+  }
+  switch (type) {
+    case "hours":
+      result = result * HOURS_IN_DAY;
+      break;
+    case "minutes":
+      result = result * MINUTES_IN_DAY;
+      break;
+    case "seconds":
+      result = result * SECONDS_IN_DAY;
+      break;
+  }
+  return `${result} ${type}`;
+}
+
+//Вивід результатів по кліку на кнопку розрахунку
+
+buttonCount.addEventListener("click", function calculateDateDifference() {
+  if (typeDate.value === "weekday") {
     output.innerHTML = getWorkingDays(
       new Date(firstDate.value),
       new Date(secondDate.value),
@@ -103,61 +173,14 @@ buttonCount.onclick = function calculateDateDifference() {
     );
   }
 
-  if (document.querySelector(".type_date").value === "weekend") {
-    function getWeekends(start, end, type) {
-      let result = 0;
-      while (start <= end) {
-        let weekDay = start.getDay();
-        if (weekDay === 0 || weekDay === 6) {
-          result++;
-        }
-        start.setDate(start.getDate() + 1);
-      }
-      switch (type) {
-        case "hours":
-          result = result * HOURS_IN_DAY;
-          break;
-        case "minutes":
-          result = result * MINUTES_IN_DAY;
-          break;
-        case "seconds":
-          result = result * SECONDS_IN_DAY;
-          break;
-      }
-      return `${result} ${type}`;
-    }
+  if (typeDate.value === "weekend") {
     output.innerHTML = getWeekends(
       new Date(firstDate.value),
       new Date(secondDate.value),
       document.querySelector(".number_date").value
     );
   }
-  if (document.querySelector(".type_date").value === "all") {
-    function durationBetweenDates(start, end, type) {
-      let x = new Date(start);
-      let y = new Date(end);
-
-      let diff = y.getTime() - x.getTime();
-      let result;
-
-      switch (type) {
-        case "days":
-          result = diff / DAY_IN_MILLISECONDS + 1;
-          break;
-        case "hours":
-          result = diff / HOUR_IN_MILLISECONDS + HOURS_IN_DAY;
-          break;
-        case "minutes":
-          result = diff / MINUTE_IN_MILLISECONDS + MINUTES_IN_DAY;
-          break;
-        case "seconds":
-          result = diff / SECOND_IN_MILLISECONDS + SECONDS_IN_DAY;
-          break;
-      }
-
-      return `${result} ${type}`;
-    }
-
+  if (typeDate.value === "all") {
     output.innerHTML = durationBetweenDates(
       new Date(firstDate.value),
       new Date(secondDate.value),
@@ -179,25 +202,29 @@ buttonCount.onclick = function calculateDateDifference() {
     end: new Date(secondDate.value).toDateString(),
     result: output.value,
   };
-  addResults(data.start, data.end, data.result);
+  addRow(data.start, data.end, data.result);
   storeToLocalStorage(data);
-};
+});
 
 //local storage
+
+function addRow(start, end, result) {
+  let item = `
+    <tr class="new-row">
+      <td class="startDate">${start}</td>
+      <td class="endDate">${end}</td>
+      <td class="result">${result}</td>
+    </tr>
+    `;
+  resultTable.insertAdjacentHTML("beforeend", item);
+}
+
 let resultData = [];
 let savedResults = JSON.parse(localStorage.getItem("resultData"));
 if (savedResults) {
   for (let value of savedResults) {
-    let item = `
-    <tr class="new-row">
-      <td class="startDate">${value.start}</td>
-      <td class="endDate">${value.end}</td>
-      <td class="result">${value.result}</td>
-    </tr>
-    `;
-    resultTable.insertAdjacentHTML("beforeend", item);
+    addRow(value.start, value.end, value.result);
   }
-
   resultTable.style.display = "table";
 }
 
@@ -211,15 +238,4 @@ function storeToLocalStorage(data) {
 
   savedResults.push(data);
   localStorage.setItem("resultData", JSON.stringify(savedResults));
-}
-
-function addResults(start, end, result) {
-  let item = `
-  <tr class="new-row">
-    <td class="startDate">${start}</td>
-    <td class="endDate">${end}</td>
-    <td class="result">${result}</td>
-  </tr>
-  `;
-  resultTable.insertAdjacentHTML("beforeend", item);
 }
